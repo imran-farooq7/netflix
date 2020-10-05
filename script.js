1cb6f6a1f924c5a1bf4d7f983dd09e32ccdafa61
unknown <you@example.com>
2020-10-05 06:11:35 +0500

    cleaing the code

--- script.js
+++ script.js
@@ -1,45 +1,61 @@
 window.onload = () => {
-  fetchMovies();
-  trendingMovies();
+  getOriginals();
+  getTrendingNow();
+  getTopRated();
 };
 
-function fetchMovies() {
-  fetch(
-    "https://api.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213"
-  )
+function showMovies(movies, element_selector, path_type) {
+  var moviesEl = document.querySelector(element_selector);
+  for (var movie of movies.results) {
+    var image = `
+          <img src="https://image.tmdb.org/t/p/original${movie[path_type]}"></img>
+      `;
+    moviesEl.innerHTML += image;
+  }
+}
+
+function fetchMovies(url, element_selector, path_type) {
+  fetch(url)
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-      addMovies(data);
-    });
-}
-function trendingMovies() {
-  fetch(
-    "https://api.themoviedb.org/3/trending/all/week?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213"
-  )
-    .then((response) => {
-      // return response.json();
-      return console.log(response.json());
-    })
-    .then((data) => {
-      addMovies(data);
+      showMovies(data, element_selector, path_type);
     });
 }
 
-function addMovies(movies) {
-  var moviesEl = document.querySelector(".original__movies");
-
-  for (var movie of movies.results) {
-    var image = `<img src="https://image.tmdb.org/t/p/original${movie.poster_path}"></img>`;
-    moviesEl.innerHTML += image;
-  }
-}
-// function trending(movies) {
-//   var trendingEl = document.querySelector(".movies__container");
+// function addMovies(movies) {}
+// function showTrend(movies) {
+//   var trendingEl = document.querySelector("#trending");
 
 //   for (var movie of movies.results) {
-//     var image = `<img src="https://image.tmdb.org/t/p/trending/all/week${movie.poster_path}"></img>`;
+//     var image = `<img src="https://image.tmdb.org/t/p/original${movie.backdrop_path}"></img>`;
 //     trendingEl.innerHTML += image;
 //   }
 // }
+
+// function showRated(movies) {
+//   var ratedEl = document.querySelector("#rated");
+
+//   for (var movie of movies.results) {
+//     var image = `<img src="https://image.tmdb.org/t/p/original${movie.backdrop_path}"></img>`;
+//     ratedEl.innerHTML += image;
+//   }
+// }
+function getOriginals() {
+  var url =
+    "https://api.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213";
+  fetchMovies(url, ".original__movies", "poster_path");
+}
+
+function getTrendingNow() {
+  var url =
+    "https://api.themoviedb.org/3/trending/movie/week?api_key=19f84e11932abbc79e6d83f82d6d1045";
+  fetchMovies(url, "#trending", "backdrop_path");
+}
+
+function getTopRated() {
+  var url =
+    "https://api.themoviedb.org/3/movie/top_rated?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US&page=1";
+  fetchMovies(url, "#rated", "backdrop_path");
+}
